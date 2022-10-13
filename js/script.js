c7701dd63d2b893c5e3818ab5f93760693eda801
IvanVNikolaev <108685107+IvanVNikolaev@users.noreply.github.com>
2022-10-13 19:57:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,105 @@
-const goods = [
-    { title: 'Pulp Fiction', price: 4500 },
-    { title: 'Death Proof', price: 2750 },
-    { title: 'Django Unchained', price: 6900 },
-    { title: 'Kill Bill', price: 3250 },
-];
 
-const renderGoodsItem = (item, img = 'img/no.svg') => {
-    return `<div class="goods-item">
-                <img src="${img}" alt="goods_image">
-                <h3>${item.title}</h3>
-                <p><span>&#36;</span>${item.price}</p>
+
+// ----------------ДЗ БУДЕТ ГОТОВО 15.10 ----------------
+// ----------------ДЗ БУДЕТ ГОТОВО 15.10 ----------------
+// ----------------ДЗ БУДЕТ ГОТОВО 15.10 ----------------
+
+
+
+
+
+
+
+
+
+
+
+
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+function makeGETRequest(url, callback) {
+    var xhr;
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            callback(xhr.responseText);
+        }
+    }
+    xhr.open('GET', url, true);
+    xhr.send();
+}
+
+class goodsList {
+    constructor(container = '.goods-list') {
+        this.container = container;
+        this.goods = [];
+        this.allGoods = [];
+        this.fetchGoods();
+        this._render();
+    }
+
+    fetchGoods(cb) {
+        // this.goods = [
+        //     { product_name: 'Pulp Fiction', price: 4500 },
+        //     { product_name: 'Death Proof', price: 2750 },
+        //     { product_name: 'Django Unchained', price: 6900 },
+        //     { product_name: 'Kill Bill', price: 3250 },
+        // ];
+        makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+            this.goods = JSON.parse(goods);
+            cb();
+        })
+    }
+
+    _render() {
+        const block = document.querySelector(this.container);
+        for (let good of this.goods) {
+            const goodObject = new GoodItem(good);
+            this.allGoods.push(goodObject);
+            block.insertAdjacentHTML('beforeend', goodObject.render());
+        }
+    }
+
+    sum() {
+        let sum = 0;
+        for (let item of this.goods) {
+            sum += item.price;
+        }
+        return sum;
+    }
+}
+
+class GoodItem {
+    constructor(good, img = 'img/no.svg') {
+        this.product_name = good.product_name;
+        this.price = good.price;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="goods-item">
+                <img src="${this.img}" alt="goods_image">
+                <h3>${this.product_name}</h3>
+                <p><span>&#36;</span>${this.price}</p>
                 <button class="goods_btn">Add to Cart</button>
             </div>`;
-};
+    }
+}
 
-const renderGoodsList = list => {
-    let goodsList = list.map(item => renderGoodsItem(item));
-    document.querySelector('.goods-list').insertAdjacentHTML('beforeend', goodsList.join(""));
-};
 
-renderGoodsList(goods);
+
+const list = new goodsList();
+
+list.fetchGoods(() => {
+    list._render();
+});
+
+
+// list.fetchGoods();
+// list._render();
+
+console.log("Total " + list.sum());
