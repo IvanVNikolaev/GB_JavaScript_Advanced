8ed909340d6aeb72e992edefbb07a0683affa38e
IvanVNikolaev <108685107+IvanVNikolaev@users.noreply.github.com>
2022-10-17 19:29:37 +0300

    Delete 1-3 directory

--- 1-3/quotes.js
+++
@@ -1,6 +0,0 @@
-let str = prompt("Введите текст с одинарными кавычками и апострофом в слове", ["The postman said, 'I won't deliver this letter tomorrow.'"]);
-let regexp1 = /'\s{1}/g;
-let regexp2 = /\s{1}'/g;
-let str2 = str.replace(regexp1, '" ').replace(regexp2, ' "');
-alert(str2);
-console.log(str2);
